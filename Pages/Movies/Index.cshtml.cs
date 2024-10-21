03d8d6659170a92fa50e43954a6a0f0eee70fd6a
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2024-10-20 19:39:16 -0500

    Adding ability to filter the list of movies by title

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -21,8 +22,18 @@ public class IndexModel : PageModel
 
     public IList<Movie> Movie { get;set; } = default!;
 
+    [BindProperty(SupportsGet = true)]
+    public string? SearchString { get; set; }
+
     public async Task OnGetAsync()
     {
-        Movie = await _context.Movie.ToListAsync();
+        var movies = from m in _context.Movie
+                     select m;
+        if (!string.IsNullOrEmpty(SearchString))
+        {
+            movies = movies.Where(m => m.Title.Contains(SearchString));
+        }
+
+        Movie = await movies.ToListAsync();
     }
 }
