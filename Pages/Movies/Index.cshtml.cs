1e9b25c4535d4b6eb797cca2cc47da5069f052d8
misirlou-tg <39652424+misirlou-tg@users.noreply.github.com>
2024-10-20 20:45:31 -0500

    Adding genre filter to list of movies

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -25,8 +25,17 @@ public class IndexModel : PageModel
     [BindProperty(SupportsGet = true)]
     public string? SearchString { get; set; }
 
+    public SelectList? Genres { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public string? MovieGenre { get; set; }
+
     public async Task OnGetAsync()
     {
+        IQueryable<string> genreQuery = from m in _context.Movie
+                                        orderby m.Genre
+                                        select m.Genre;
+
         var movies = from m in _context.Movie
                      select m;
         if (!string.IsNullOrEmpty(SearchString))
@@ -34,6 +43,12 @@ public class IndexModel : PageModel
             movies = movies.Where(m => m.Title.Contains(SearchString));
         }
 
+        if (!string.IsNullOrEmpty(MovieGenre))
+        {
+            movies = movies.Where(m => m.Genre == MovieGenre);
+        }
+
+        Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
         Movie = await movies.ToListAsync();
     }
 }
